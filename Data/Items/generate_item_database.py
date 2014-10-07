8341e0fb7dfec21e1615fe400f3d92e2cb830da2
Michael Bao <halo343gs@gmail.com>
2014-10-06 23:07:19 -0700

    regenerate database xml file for new items

--- Data/Items/generate_item_database.py
+++ Data/Items/generate_item_database.py
@@ -1,34 +1,20 @@
-import sys
-import os
 import xml.etree.ElementTree as ET
 import re
+import json
 from xml.dom import minidom
 
-items_image_path = os.getcwd() + "/Images/"
-item_regex = re.compile("(\d+)_ImagePack_items_Embeds__e_(\d+)_(.+).png")
+jsonInputFile = open("items.json0", "r")
+jsonInput = json.loads(jsonInputFile.read())
 
 database = ET.Element("database")
-allItems = ET.SubElement(database, "items")
+items = ET.SubElement(database, "items")
 
-for root, dirs, files in os.walk(items_image_path):
-	for name in files:
-		res = re.match(item_regex, name)
-		if res == None:
-			continue
-		
-		itemCount = res.group(1)
-		itemId = res.group(2)
-		itemName = res.group(3)
-
-		newItem = ET.SubElement(allItems, "item")
-		itemCountEle = ET.SubElement(newItem, "count")
-		itemCountEle.text = itemCount
-
-		itemIdEle = ET.SubElement(newItem, "id")
-		itemIdEle.text = itemId
-
-		itemNameEle = ET.SubElement(newItem, "name")
-		itemNameEle.text = itemName
+for itemObject in jsonInput["items"]:
+  newItem = ET.SubElement(items, "item")
+  idEle = ET.SubElement(newItem, "id")
+  idEle.text = itemObject["id"]
+  pathEle = ET.SubElement(newItem, "path")
+  pathEle.text = itemObject["icon"].replace(".dds", ".png")
 
 xmlText = ET.tostring(database, 'utf-8')
 xmlTextParse = minidom.parseString(xmlText)
